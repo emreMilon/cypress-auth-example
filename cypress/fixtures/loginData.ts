2f93d2f29285f818fc6dbddf13338e2881b6d3e6
emreMilon <kurt@milon.com>
2022-01-11 16:32:55 +0100

    edit_ json files updated to ts files

---
+++ cypress/fixtures/loginData.ts
@@ -0,0 +1,15 @@
+import { ILoginData } from "../support/interfaces";
+
+export const loginData : ILoginData[] = [
+  {
+    // position: leiter
+    "email": "john@milon.com",
+    "password": "12345678"
+  },
+    {
+      // position: vertrieber
+      "email": "max@milon.com",
+      "password": "12345678"
+    }
+]
+  
